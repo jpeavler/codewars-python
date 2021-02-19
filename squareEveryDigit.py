5ac6e8b4803ea07821d0417055eb2659744b7f74
Joshua Peavler <joshuaapeavler@gmail.com>
2021-02-19 16:01:45 -0700

    Added comments to square every digit exercise.

--- squareEveryDigit.py
+++ squareEveryDigit.py
@@ -1,3 +1,4 @@
+#square_digits takes in a number, squares every digit of that number and returns that number
 def square_digits(num):
-    digits = [str(int(digit)**2) for digit in str(num)] 
-    return int("".join(digits))
+    digits = [str(int(digit)**2) for digit in str(num)] #makes a list of strings representing the squared value of each digit in the original number
+    return int("".join(digits)) #combines all those strings into a number
